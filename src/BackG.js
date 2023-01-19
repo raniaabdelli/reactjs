838558c9da07c7bcf6c4162f823f161d848014ef
rania abdelli <abdellirania1@gmail.com>
2023-01-19 16:26:13 +0100

    envoiCheckpointREACT

---
+++ src/BackG.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import Pic from '../BG.jpeg'
+
+const BackG = () => {
+  return (
+    <img src="/BG.jpeg"/>
+    //<div style={{ backgroundImage:`url(${BG.jpeg})` }}>BackG</div>
+  )
+}
+
+export default BackG
+
+
+
